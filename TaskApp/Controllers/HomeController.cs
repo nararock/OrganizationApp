8ec5a79b0454a2268ec472fb9c354015b2058691
Elena Gerasimovich <nararock@mail.ru>
2024-04-09 23:47:12 +0300

    1. Главная страница пересена на страницу со списком организаций. Перенесены ссылки с главной страницы.

--- TaskApp/Controllers/HomeController.cs
+++ TaskApp/Controllers/HomeController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TaskApp.Classes;
+using TaskApp.Models;
+using TaskApp.TaskDb;
 
 namespace TaskApp.Controllers
 {
     public class HomeController : Controller
     {
+        private TaskContext TaskContext;
+        public HomeController(TaskContext taskContext)
+        {
+            TaskContext = taskContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            EmployeeHelper employeeHelper = new EmployeeHelper();
+            List<OrganizationModel> organizations = employeeHelper.getOrganizations(TaskContext);
+            return View(organizations);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Response response = new Response
+            {
+                State = true,
+            };
+            return View(response);
+        }
+
+        [HttpPost]
+        public IActionResult Create(OrganizationCreateModel organization)
+        {
+            OrganizationHelper organizationHelper = new OrganizationHelper();
+            Response response = organizationHelper.CreateNewOrganization(organization, TaskContext);
+            if (response.State)
+                return Redirect("/Organization/Index");
+            return View(response);
         }
     }
 }
